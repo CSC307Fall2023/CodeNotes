c69eeada4ca0f0eacb00622abb6c161256f8bae3
Kavya Karunakaran <90122782+kavya-vk@users.noreply.github.com>
2023-10-29 09:18:08 -0700

    pulled changes to folders from Thursday from main, added notebook buttons

--- src/app/userprofile/page.js
+++ src/app/userprofile/page.js
@@ -1,7 +1,9 @@
 'use client'
 
 import Box from '@mui/material/Box';
-
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+//import { styled } from '@material-ui/core/styles';
 
 
 export default function Note() {
@@ -15,6 +17,7 @@ export default function Note() {
         <>
             <h2>CSC 307</h2>
             <h1>HTML and JS Introduction</h1>
+            <ButtonRow/>
         
         <Box sx={boxStyle}>
 
@@ -22,3 +25,41 @@ export default function Note() {
         </>
     )
 }
+
+function ButtonRow() {
+    //const classes = notebookButtonStyles();
+
+    return (
+      <Grid container spacing={2}>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            Notebook 1
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            Notebook 2
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            Notebook 3
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+/*
+const notebookButtonStyles = styled(Button)(( { theme }) => ({
+    customButton: {
+        backgroundColor: 'white', 
+        color: 'black', 
+        border: '1px solid grey', 
+        '&:hover': {
+            backgroundColor: 'grey', 
+            color: 'white'
+        },
+    },
+}));
+*/

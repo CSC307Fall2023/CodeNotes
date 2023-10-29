d28ea6a759b1fd4ab509a46f0428f77af78df29a
Mira Shlimenzon <tigragoldina@gmail.com>
2023-10-29 14:50:19 -0700

    updated code (got elements out)

--- src/app/notes/page.js
+++ src/app/notes/page.js
@@ -1,24 +1,55 @@
 'use client'
-
+import React from 'react';
 import Box from '@mui/material/Box';
 
+const folderBarStyle = {
+  backgroundColor: '#D9D9D9',
+  height: '100vh', // 100% of the viewport height
+  width: '35vh'
+};
 
+const contentContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column'
+};
+
+const headerStyle = {
+  height: '150px',
+  padding: '0 20px'
+};
+
+const classNotebookLabelStyle = {
+  fontFamily: 'Inter-SemiBold, Helvetica',
+  fontSize: '20px',
+  color: '#050505',
+  margin: '0',
+  marginTop: '40px'
+};
+
+const noteLabelStyle = {
+  fontFamily: 'Inter-SemiBold, Helvetica',
+  fontSize: '40px',
+  color: '#050505',
+  margin: '0'
+};
+
+const toolBoxStyle = {
+  backgroundColor: '#D9D9D9',
+  height: '60px',
+  width: '100vh'
+};
 
 export default function Note() {
-    const boxStyle = {
-        backgroundColor: '#D9D9D9', // Gray color
-        height: '2px', // Adjust the height as needed
-        width: '100%', // Span the full width of the screen
-    };
-
-    return (
-        <>
-            <h2>CSC 307</h2>
-            <h1>HTML and JS Introduction</h1>
-        
-        <Box sx={boxStyle}>
-
-        </Box>
-        </>
-    )
+  return (
+    <div style={{ display: 'flex' }}>
+      <Box sx={folderBarStyle}></Box>
+      <div style={contentContainerStyle}>
+        <div style={headerStyle}>
+          <h2 style={classNotebookLabelStyle}>    CSC 307</h2>
+          <h1 style={noteLabelStyle}>    HTML and JS Introduction</h1>
+        </div>
+        <Box sx={toolBoxStyle}></Box>
+      </div>
+    </div>
+  );
 }

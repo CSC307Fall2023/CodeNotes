fdcce33f5dbd7c7836798f3b1fcd16fb534b664c
jakethejrm <73371286+jakethejrm@users.noreply.github.com>
2023-10-29 09:16:04 -0700

    Landing Page Layout Done

--- src/app/RootLayout.js
+++ src/app/RootLayout.js
@@ -13,10 +13,122 @@ import Login from './Login';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Signup from './Signup';
 import { useSession } from 'next-auth/react';
-import { Button } from '@mui/material';
+import { Button, Divider } from '@mui/material';
 import { signOut } from "next-auth/react"
 
-const theme = createTheme({});
+const theme = createTheme({palette: {
+  mode: 'light',
+  primary: {
+    main: '#154734',
+  },
+  secondary: {
+    main: '#BD8B13',
+  },
+},
+typography: {
+  fontFamily: 'sans-serif',
+  fontWeightLight: 300,
+  fontWeightRegular: 400,
+  fontWeightMedium: 500,
+  fontWeightBold: 700,
+  h1: {
+    fontSize: '3rem',
+    fontWeight: 700,
+  },
+  h2: {
+    fontSize: '2.5rem',
+    fontWeight: 700,
+  },
+  h3: {
+    fontSize: '2rem',
+    fontWeight: 700,
+  },
+  h4: {
+    fontSize: '1.5rem',
+    fontWeight: 700,
+  },
+  h5: {
+    fontSize: '1.25rem',
+    fontWeight: 700,
+  },
+  h6: {
+    fontSize: '1rem',
+    fontWeight: 700,
+  },
+  subtitle1: {
+    fontSize: '1rem',
+    fontWeight: 400,
+  },
+  subtitle2: {
+    fontSize: '0.875rem',
+    fontWeight: 400,
+  },
+  body1: {
+    fontSize: '1rem',
+    fontWeight: 400,
+  },
+  body2: {
+    fontSize: '0.875rem',
+    fontWeight: 400,
+  },
+  button: {
+    fontSize: '1rem',
+    fontWeight: 700,
+  },
+  caption: {
+    fontSize: '0.75rem',
+    fontWeight: 400,
+  },
+  overline: {
+    fontSize: '0.75rem',
+    fontWeight: 400,
+  },
+
+
+},
+components: {
+  MuiDivider: {
+    styleOverrides: {
+      root: {
+        backgroundColor: '#EEEEEE'
+      },
+    },
+  },
+  MuiAppBar: {
+    styleOverrides: {
+      root: {
+        backgroundColor: '#FFFFFF',
+        color: '#000000',
+        boxShadow: 'none',
+      },
+    },
+  },
+  MuiButton: {
+    styleOverrides: {
+      root: {
+        textTransform: 'none',
+        fontWeight: 700,
+      },
+      contained: {
+        backgroundColor: "#154734",
+        color: "#FFFFFF",
+        '&:hover': {
+          backgroundColor: "#154734",
+        },
+      },
+    },
+  },
+  MuiToolbar: {
+    styleOverrides: {
+      root: {
+        backgroundColor: '#FFFFFF',
+        color: '#000000',
+        
+      },
+    },
+  },
+  }
+});
 
 export default function RootLayout({ children, title }) {
 
@@ -29,6 +141,7 @@ export default function RootLayout({ children, title }) {
   } else {
     loginSection = <>
       <Login/>
+      <Divider orientation="vertical" variant="middle" flexItem ></Divider>
       <Signup/>
     </>;
   }
@@ -37,21 +150,20 @@ export default function RootLayout({ children, title }) {
     <ThemeProvider theme={theme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="static">
-          <Container maxWidth="xl">
+        <AppBar position="static" sx={{paddingX: 3}}>
             <Toolbar disableGutters>
-              <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+              <img src="/CalPoly.png" alt="logo" height="45" position="inline-block" style={{ marginRight : "10px"}} />
+              <Divider orientation="vertical" variant="middle" flexItem ></Divider>
+              <img src="/logo.png" alt="logo" width="40" height="40" style={{marginLeft : "10px"}} />
               <Typography
-                variant="h6"
+                variant="h4"
                 noWrap
                 component="a"
                 href="/"
                 sx={{
-                  mr: 2,
+                  m: 2,
                   display: { xs: 'none', md: 'flex' },
-                  fontFamily: 'monospace',
                   fontWeight: 700,
-                  letterSpacing: '.3rem',
                   color: 'inherit',
                   textDecoration: 'none',
                 }}
@@ -65,7 +177,6 @@ export default function RootLayout({ children, title }) {
                 </Stack>
               </Box>
             </Toolbar>
-          </Container>
         </AppBar>
       </Box>
       <Box component="main" sx={{ p: 3 }}>

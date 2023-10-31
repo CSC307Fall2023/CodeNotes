f5fe88bc65a41602925ceccd7d818736b7dca3c1
Sophie Russ <sophieruss2002@gmail.com>
2023-10-31 12:15:08 -0700

    user profile

--- src/app/userprofile/page.js
+++ src/app/userprofile/page.js
@@ -1,9 +1,10 @@
 'use client'
 
 import Box from '@mui/material/Box';
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
-
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
 
 const boxStyle = {
     backgroundColor: '#D9D9D9', // Gray color
@@ -15,17 +16,30 @@ const boxStyle = {
     alignItems: 'center', // Center horizontally
 };
 
-
 const avatar = {
     width: 250,
     height: 250,
 };
 
+
+
 export default function Note() {
-    
+    const [pic, changePic] = useState("profilePicture.jpg");
+
+    const handleClick = () => {
+        if (pic == "profilePicture.jpg")
+            changePic("profilePicture2.jpg")
+        else
+            changePic("profilePicture.jpg")
+        
+
+    };
     return (
         <Box sx={boxStyle}>
-            <Avatar sx={avatar} alt="Profile Picture" src="profilePicture.jpeg" />
+
+            <IconButton color="secondary" aria-label="Edit" onClick={handleClick}>
+                <Avatar sx={avatar} variant="circular" alt="Profile Picture" src={pic} />
+            </IconButton>
 
             <h1>Name</h1>
             <h2>Computer Science 2025</h2>

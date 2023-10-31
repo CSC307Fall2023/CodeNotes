a4272f59dbd2b47bbed43a3e908a7ad3f8f5751a
Sophie Russ <sophieruss2002@gmail.com>
2023-10-31 10:57:54 -0700

    tried to add photo

--- src/app/userprofile/page.js
+++ src/app/userprofile/page.js
@@ -1,24 +1,38 @@
 'use client'
 
 import Box from '@mui/material/Box';
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
 
 
+const boxStyle = {
+    backgroundColor: '#D9D9D9', // Gray color
+    height: '2px', // Adjust the height as needed
+    width: '100%', // Span the full width of the screen
+
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center', // Center horizontally
+};
+
+
+    
+
+const avatar = {
+    width: 250,
+    height: 250,
+};
 
 export default function Note() {
-    const boxStyle = {
-        backgroundColor: '#D9D9D9', // Gray color
-        height: '2px', // Adjust the height as needed
-        width: '100%', // Span the full width of the screen
-    };
-
+    
     return (
-        <>
-            <h2>CSC 307</h2>
-            <h1>HTML and JS Introduction</h1>
-        
         <Box sx={boxStyle}>
+            <Avatar sx={avatar} alt="MProfile Picture" src="profilePicture.jpeg" />
 
+            <h1>Name</h1>
+            <h2>Computer Science 2025</h2>
+        
         </Box>
-        </>
+        
     )
 }

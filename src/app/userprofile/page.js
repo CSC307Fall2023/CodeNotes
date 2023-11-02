ba483fe53a11b066ab1545b2e853070d97f7469d
jakethejrm <73371286+jakethejrm@users.noreply.github.com>
2023-11-02 03:14:29 -0700

    Landing Page, NavBar Revamp, Theming
    -Fixed bug with Link component in userprofile
    - Fully added Navbar that is visible everywhere
    - Fully functional Landing Page

--- src/app/userprofile/page.js
+++ src/app/userprofile/page.js
@@ -1,10 +1,11 @@
 'use client'
 
-import Box from '@mui/material/Box';
-import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
+import Box from '@mui/material/Box'
+import React, { useState } from 'react'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import EditIcon from '@mui/icons-material/Edit'
+import Link from 'next/link'
 
 const boxStyle = {
     backgroundColor: '#D9D9D9', // Gray color
@@ -14,43 +15,46 @@ const boxStyle = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center', // Center horizontally
-};
+}
 
 const avatar = {
     width: 250,
     height: 250,
-};
-
-
+}
 
 export default function Note() {
-    const [pic, changePic] = useState("profilePicture.jpg");
+    const [pic, changePic] = useState('profilePicture.jpg')
 
     const handleClick = () => {
-        if (pic == "profilePicture.jpg")
-            changePic("profilePicture2.jpg")
-        else
-            changePic("profilePicture.jpg")
-    };
+        if (pic == 'profilePicture.jpg') changePic('profilePicture2.jpg')
+        else changePic('profilePicture.jpg')
+    }
 
     return (
         <>
-        
             <h2>CSC 307</h2>
             <h1>User Profile</h1>
 
-            <h2><Link href="./notes">Note</Link></h2>
-        <Box sx={boxStyle}>
+            <h2>
+                <Link href="./notes">Note</Link>
+            </h2>
+            <Box sx={boxStyle}>
+                <IconButton
+                    color="secondary"
+                    aria-label="Edit"
+                    onClick={handleClick}
+                >
+                    <Avatar
+                        sx={avatar}
+                        variant="circular"
+                        alt="Profile Picture"
+                        src={pic}
+                    />
+                </IconButton>
 
-            <IconButton color="secondary" aria-label="Edit" onClick={handleClick}>
-                <Avatar sx={avatar} variant="circular" alt="Profile Picture" src={pic} />
-            </IconButton>
-
-            <h1>Name</h1>
-            <h2>Computer Science 2025</h2>
-        
-        </Box>
-
+                <h1>Name</h1>
+                <h2>Computer Science 2025</h2>
+            </Box>
         </>
     )
 }

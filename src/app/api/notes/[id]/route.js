1b6f03cb5fef9f8f99210f43c49640f2684feb03
Mira Shlimenzon <tigragoldina@gmail.com>
2023-11-20 19:46:23 -0500

    update for jacob to check

--- src/app/api/notes/[id]/route.js
+++ src/app/api/notes/[id]/route.js
@@ -1,9 +1,5 @@
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/db'
-import { checkLoggedIn } from '@/lib/auth'
-
-// look into middleware - checks before each call that user is logged in !!!
-// put matches in and paths matcher: put path in the matcher
 
 function exclude(user, keys) {
     return Object.fromEntries(
@@ -13,8 +9,6 @@ function exclude(user, keys) {
 
 // get pre-existing note with certain id
 export async function GET(request, { params }) {
-    //const loggedInData = await checkLoggedIn() // to update this in every single API call we have
-    //if (loggedInData.loggedIn) {
     const id = parseInt(params.id)
     if (id) {
         const note = await prisma.note.findUnique({
@@ -24,27 +18,37 @@ export async function GET(request, { params }) {
         })
         return NextResponse.json(note)
     }
-    //}
 }
-
+  
 // update note with potentially new content or a new title or new notebook id
 // other terms to call this call is: write to note or rename note or move note to new notebook
 export async function PATCH(request, { params }) {
     const id = parseInt(params.id)
-
     if (id) {
         const { title, content, notebookId } = await request.json()
+
+        // getting current note information 
+        const currentNote = await prisma.note.findUnique({
+            where: {
+              id,
+            },
+            select: {
+                notebookId: true,
+                title: true,
+                content: true,
+            },
+        });
 
         const note = await prisma.note.update({
             where: {
                 id,
             },
             data: {
-                title: title || undefined,
-                content: content || undefined,
+                title: title ?? currentNote.title,
+                content: content ?? currentNote.content,
                 notebook: notebookId
-                    ? { connect: { id: notebookId } }
-                    : undefined,
+                ? { connect: { id: notebookId } }
+                : { connect: { id: currentNote.notebookId } },
             },
         })
         return NextResponse.json(note)

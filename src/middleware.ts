aa50142850dc7e6b1ba97c259eb507b2e458ddad
jakethejrm <73371286+jakethejrm@users.noreply.github.com>
2023-12-05 23:38:08 -0800

    ROUTE PROTECTION BABEY

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,42 @@ import { withAuth } from 'next-auth/middleware'
 // middleware is applied to all routes, use conditionals to select
 
 export default withAuth(function middleware(req) {}, {
-    pages: {
-        signIn: '',
-    },
     callbacks: {
         authorized: ({ req, token }) => {
-            if (req.nextUrl.pathname.startsWith('/account') && token === null) {
+            if (
+                req.nextUrl.pathname.startsWith('/api/account') &&
+                token === null
+            ) {
+                return false
+            }
+            if (
+                req.nextUrl.pathname.startsWith('/api/classes') &&
+                token === null
+            ) {
+                return false
+            }
+            if (
+                req.nextUrl.pathname.startsWith('/api/notebooks') &&
+                token === null
+            ) {
+                return false
+            }
+            if (
+                req.nextUrl.pathname.startsWith('/api/notes') &&
+                token === null
+            ) {
+                return false
+            }
+            if (
+                req.nextUrl.pathname.startsWith('/api/profile') &&
+                token === null
+            ) {
+                return false
+            }
+            if (
+                req.nextUrl.pathname.startsWith('/api/users') &&
+                token === null
+            ) {
                 return false
             }
             return true
